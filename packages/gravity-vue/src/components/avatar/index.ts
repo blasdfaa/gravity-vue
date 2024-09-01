b6963de885ce2e65848886f09c69f39d0e1ebe12
Viktor Zdorovtsev <v.zdvacew@gmail.com>
2024-09-01 12:33:27 +0700

    feat: implement component

---
+++ packages/gravity-vue/src/components/avatar/index.ts
@@ -0,0 +1,5 @@
+export {
+  default as Avatar,
+  type AvatarProps,
+} from './avatar.vue'
+export type { AvatarSize, AvatarTheme, AvatarView } from './types'
---
+++ packages/gravity-vue/src/components/avatar/types.ts
@@ -0,0 +1,3 @@
+export type AvatarSize = 'xs' | 's' | 'm' | 'l' | 'xl'
+export type AvatarTheme = 'normal' | 'brand'
+export type AvatarView = 'filled' | 'outlined'
