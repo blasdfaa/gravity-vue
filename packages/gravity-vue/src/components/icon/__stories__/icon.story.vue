36598d42d42e51e1a407c3ba1adab954d46d8e15
Ernest Manukyan <75063653+brokuka@users.noreply.github.com>
2024-09-03 18:39:31 +0400

    feat: adding icon component (#52)

---
+++ packages/gravity-vue/src/components/icon/__stories__/icon.story.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Envelope, Gear, Rocket } from '@gravity-vue/icons'
+
+import Icon from '../icon.vue'
+
+const icons = [Envelope, Gear, Rocket]
+</script>
+
+<template>
+  <Story group="components" title="Data Display / Icon">
+    <Variant title="Basic">
+      <Icon v-for="(icon, index) in icons" :key="index" :data="icon" size="16" />
+    </Variant>
+  </Story>
+</template>
---
+++ packages/gravity-vue/src/components/icon/icon.vue
@@ -0,0 +1,65 @@
+<script lang="ts">
+import type { QAProps } from '../../types'
+import { computed } from 'vue'
+import type { IconData, SVGDementions } from './types'
+import { useBlock } from '../../composables'
+import { getDementions, isComponentSvgData, isSpriteData, isStringSvgData, prepareStringData } from './utils'
+
+export interface IconProps extends QAProps, SVGDementions {
+  data: IconData
+  fill?: string
+  stroke?: string
+  class?: string
+}
+</script>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<IconProps>(), {
+  fill: 'currentColor',
+  stroke: 'none',
+})
+
+const { b } = useBlock('icon')
+
+const dimensions = computed(() => getDementions(props))
+
+const svgProps = computed(() => ({
+  'xmlns': 'http://www.w3.org/2000/svg',
+  'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+  'width': dimensions.value.width,
+  'height': dimensions.value.height,
+  'class': b(null, props.class),
+  'fill': props.fill,
+  'stroke': props.stroke,
+  'data-qa': props.qa,
+}))
+
+const href = computed(() => {
+  if (isSpriteData(props.data)) {
+    return (props.data.url || `#${props.data.id}`)
+  }
+  return ''
+})
+</script>
+
+<template>
+  <svg v-if="isStringSvgData(data)" v-bind="svgProps" v-html="prepareStringData(data)" />
+
+  <component :is="data" v-if="isComponentSvgData(data)" v-bind="svgProps" />
+
+  <svg v-else-if="isSpriteData(data)" v-bind="svgProps">
+    <use :href="href" :xlink:href="href" />
+  </svg>
+</template>
+
+<style lang="scss">
+@use '../../styles/variables';
+
+$block: '.#{variables.$ns}icon';
+
+#{$block} {
+  color: inherit;
+  line-height: 0;
+  vertical-align: top;
+}
+</style>
