35aadc4c5b1f3d57f044875f470f29a2a2e2a2bc
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-09-05 21:30:39 +0400

    refactor: update module

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -10,8 +10,15 @@ export default defineNuxtModule<ModuleOptions>({
   defaults: {
     css: true,
   },
-  // eslint-disable-next-line unused-imports/no-unused-vars
   async setup(options, nuxt) {
+    if (options.css) {
+      nuxt.options.css.push('gravity-vue/styles.css')
+    }
+
+    nuxt.options.vite.optimizeDeps = nuxt.options.vite.optimizeDeps || {}
+    nuxt.options.vite.optimizeDeps.include = nuxt.options.vite.optimizeDeps.include || []
+    nuxt.options.vite.optimizeDeps.include.push('gravity-vue')
+
     Object.keys(components).forEach((name) => {
       addComponent({
         name,
@@ -20,35 +27,10 @@ export default defineNuxtModule<ModuleOptions>({
       })
     })
 
-    // Рабочий вариант
-    // const componentNames = Object.keys(components)
-
-    // componentNames.push('ThemeProvider')
-
-    // if (options.css) {
-    //   nuxt.options.css.push('gravity-vue/styles.css')
-    // }
-
-    // const gravityVuePath = dirname(require.resolve('gravity-vue/package.json'))
-
-    // try {
-    //   componentNames.forEach((name) => {
-    //     const nameAsKebabCase = kebabCase(name)
-    //     const componentPath = name === 'ThemeProvider'
-    //       ? join(gravityVuePath, 'src', 'providers', 'theme')
-    //       : join(gravityVuePath, 'src', 'components', nameAsKebabCase)
-
-    //     addComponent({
-    //       name,
-    //       export: name,
-    //       filePath: componentPath,
-    //     })
-    //   })
-    // }
-    // catch (err) {
-    //   if (err instanceof Error) {
-    //     console.warn(`Error adding component: ${err.message}`)
-    //   }
-    // }
+    addComponent({
+      name: 'ThemeProvider',
+      export: 'ThemeProvider',
+      filePath: 'gravity-vue',
+    })
   },
 })
